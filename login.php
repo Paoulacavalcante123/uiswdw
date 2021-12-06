6e468df2a7fb45bee8a72a9f23e85568ad8d8fda
Paoulacavalcante123 <93795481+Paoulacavalcante123@users.noreply.github.com>
2021-12-06 09:26:02 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">   
+    <meta name="viewport" content="width=device-width, initial-scale">
+    <title>login cliente</title>
+</head>
+<body>
+    <div class="caixa">
+        <form action="index.php" method="post">
+            <label for="login">Login</label>
+            <input type="text" name="login">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha">
+
+            <input type="submit" value="Entrar" name="en">
+        </form>
+    </div>
+</body>
+</html>
